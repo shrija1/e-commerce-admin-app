46fc70be2d9c3eb256c7cb2dab2435b17ed0d136
shrija1 <shrija.hit.ece22@heritageit.edu.in>
2021-07-06 14:10:49 +0530

    admin app

---
+++ src/containers/Home/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Layout from "../../components/Layout";
+import { Jumbotron } from "react-bootstrap";
+/**
+ * @author
+ * @function Home
+ **/
+
+const Home = (props) => {
+  return (
+    <Layout sidebar>
+      <Jumbotron
+        style={{ margin: "5rem", background: "#fff" }}
+        className="text-center"
+      >
+        <h1>Welcome to Admin Dashboard</h1>
+      </Jumbotron>
+    </Layout>
+  );
+};
+
+export default Home;
